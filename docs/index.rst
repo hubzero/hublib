e9a95fa186c08f0a189b55547058e20d4398c9fd
Martin Hunt <huntmartinm@gmail.com>
2016-10-26 00:43:19 -0400

    Add docs framework

---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+.. hublib documentation master file, created by
+   sphinx-quickstart on Mon Oct 24 19:40:28 2016.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to hublib's documentation!
+==================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
--- setup.py
+++ setup.py
@@ -3,9 +3,76 @@ from setuptools import setup, find_packages
 import io
 import os
 import sys
-
+from setuptools.command.test import test as TestCommand
 import hublib
-print(hublib.__version__)
+
+class PyTest(TestCommand):
+    user_options = [("cov=", None, "Run coverage"),
+                    ("cov-xml=", None, "Generate junit xml report"),
+                    ("cov-html=", None, "Generate junit html report"),
+                    ("junitxml=", None, "Generate xml of test results")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.cov = None
+        self.cov_xml = False
+        self.cov_html = False
+        self.junitxml = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        if self.cov is not None:
+            self.cov = ["--cov", self.cov, "--cov-report", "term-missing"]
+            if self.cov_xml:
+                self.cov.extend(["--cov-report", "xml"])
+            if self.cov_html:
+                self.cov.extend(["--cov-report", "html"])
+        if self.junitxml is not None:
+            self.junitxml = ["--junitxml", self.junitxml]
+
+    def run_tests(self):
+        try:
+            import pytest
+        except:
+            raise RuntimeError("py.test is not installed, "
+                               "run: pip install pytest")
+        params = {"args": self.test_args}
+        if self.cov:
+            params["args"] += self.cov
+        if self.junitxml:
+            params["args"] += self.junitxml
+        errno = pytest.main(**params)
+        sys.exit(errno)
+
+def sphinx_builder():
+    try:
+        from sphinx.setup_command import BuildDoc
+    except ImportError:
+        class NoSphinx(Command):
+            user_options = []
+
+            def initialize_options(self):
+                raise RuntimeError("Sphinx documentation is not installed, "
+                                   "run: pip install sphinx")
+
+        return NoSphinx
+
+    class BuildSphinxDocs(BuildDoc):
+
+        def run(self):
+            if self.builder == "doctest":
+                import sphinx.ext.doctest as doctest
+                # Capture the DocTestBuilder class in order to return the total
+                # number of failures when exiting
+                ref = capture_objs(doctest.DocTestBuilder)
+                BuildDoc.run(self)
+                errno = ref[-1].total_failures
+                sys.exit(errno)
+            else:
+                BuildDoc.run(self)
+
+    return BuildSphinxDocs
+
 
 def read(*filenames, **kwargs):
     encoding = kwargs.get('encoding', 'utf-8')
@@ -18,6 +85,11 @@ def read(*filenames, **kwargs):
 
 long_description = read('README.rst', 'CHANGES.rst')
 
+# Additional setup commands
+cmdclass = {
+    'docs': sphinx_builder(),
+    'test': PyTest
+}
 
 setup(
     name='hublib',
@@ -30,9 +102,11 @@ setup(
     author_email='mmh@purdue.edu',
     description='Python library for HUBzero Jupyter Notebooks',
     long_description=long_description,
-    packages=['hublib', 'hublib.uq'],
+    packages=['hublib', 'hublib.uq', 'hublib.ui', 'hublib.tool'],
     include_package_data=True,
     platforms='any',
+    tests_require=['pytest-cov', 'pytest'],
+    cmdclass=cmdclass,
     classifiers = [
         'Programming Language :: Python',
         'Development Status :: 3 - Alpha',
