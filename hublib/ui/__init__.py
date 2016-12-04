80b62af3791a811945da40299e4509359778f936
Martin Hunt <huntmartinm@gmail.com>
2016-12-04 11:26:08 -0500

    add FileUpload

--- hublib/ui/__init__.py
+++ hublib/ui/__init__.py
@@ -1,3 +1,4 @@
 from .numvalue import Number, Integer
 from .formvalue import String, Dropdown, Checkbox, Radiobuttons, Togglebuttons, Text
 from .group import Tab, Form
+from .upload import FileUpload
