7e79315a681655dba239c03dfc0c37df175cd0d2
Martin Hunt <mmh@purdue.edu>
2017-10-30 23:38:14 -0400

    add auth arg

--- hublib/util/__init__.py
+++ hublib/util/__init__.py
@@ -61,7 +61,7 @@ def get_proxy_addr():
             fxc = line.split()[1]
     url_path = "/weber/%s/%s/%s/" % (session, fxc, fxp)
     proxy_url = "https://proxy." + url.split('//')[1] + url_path
-    print("Connect to %s" % proxy_url, file=sys.stderr)
+    print("Connect to %s?auth=%s" % (proxy_url, get_cookie()[1]), file=sys.stderr)
     return url_path, proxy_url
 
 
@@ -76,45 +76,72 @@ if found_flask:
             self._protect_views()
             self._index_view_name = app.config['routes_pathname_prefix']
 
+        def set_cookie(self, response, name, value):
+            response.set_cookie(
+                name,
+                value=value,
+                max_age=24 * 60 * 60 * 7,
+                secure=True,
+                path=self.app.config['routes_pathname_prefix']
+            )
+
         def _overwrite_index(self):
             original_index = self.app.server.view_functions[self._index_view_name]
-
+            
             def wrap_index(*args, **kwargs):
-                if self.is_authorized():
-                    return original_index(*args, **kwargs)
-                else:
-                    return self.login_request()
+                res = original_index(*args, **kwargs)                
+                response = flask.Response(res)
+                
+                if 'auth' in flask.request.args:
+                    auth = flask.request.args['auth']
+                    # print("compare: '%s' ****** '%s'" % (auth, self.token))
+                    if auth == self.token:
+                        # print("SETTING COOKIE")
+                        self.set_cookie(response, self.cookie_name, auth)
+                        return response
+            
+                # print("cookies=", flask.request.cookies)
+                if self.cookie_name not in flask.request.cookies:
+                    # print("%s not in cookies" % self.cookie_name)
+                    return flask.Response('Access Forbidden', status=403)
+
+                cookie = flask.request.cookies[self.cookie_name]
+                cookie = url_unescape(cookie)
+                for item in cookie.split(','):
+                    # print("compare: '%s' ****** '%s'" % (item, self.token))
+                    if item == self.token:
+                        return response
+                return flask.Response('Access Forbidden', status=403)
+                
             self.app.server.view_functions[self._index_view_name] = wrap_index
 
         def _protect_views(self):
-            # TODO - allow users to white list in case they add their own views
             for view_name, view_method in iteritems(self.app.server.view_functions):
-                if view_name != self._index_view_name:
-                    self.app.server.view_functions[view_name] = \
-                        self.auth_wrapper(view_method)
+                self.app.server.view_functions[view_name] = self.auth_wrapper(view_method)
 
-        def is_authorized(self):
-            if self.cookie_name not in flask.request.cookies:
-                return False
-
-            cookie = flask.request.cookies[self.cookie_name]
-            cookie = url_unescape(cookie)
-            for item in cookie.split(','):
-                if item == self.token:
-                    return True
-            return False
-
-        def login_request(self):
-            return flask.Response(
-                'Access Forbidden',
-                status=403)
 
         def auth_wrapper(self, f):
             def wrap(*args, **kwargs):
-                if not self.is_authorized():
-                    return flask.Response(status=403)
-                response = f(*args, **kwargs)
-                return response
+                # print("auth_wrapper:")
+                # print("    cookies=", flask.request.cookies)
+                if 'auth' in flask.request.args:
+                    auth = flask.request.args['auth']
+                    if auth == self.token:
+                        # print("SETTING COOKIE 2")
+                        response = flask.Response(f(*args, **kwargs))
+                        self.set_cookie(response, self.cookie_name, auth)
+                        return f(*args, **kwargs)
+                if self.cookie_name not in flask.request.cookies:
+                    # print("%s not in cookies" % self.cookie_name)
+                    return flask.Response('Access Forbidden', status=403)
+
+                cookie = flask.request.cookies[self.cookie_name]
+                cookie = url_unescape(cookie)
+                for item in cookie.split(','):
+                    # print("    compare: '%s' ****** '%s'" % (item, self.token))
+                    if item == self.token:
+                        return f(*args, **kwargs)           
+                return flask.Response('Access Forbidden', status=403)
             return wrap
 
     def check_access(app):
