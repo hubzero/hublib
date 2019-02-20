fd873b08340e97416ee354f170763d7bc82da6ca
Martin Hunt <huntmartinm@gmail.com>
2019-02-19 23:26:54 -0500

    as_nb() and as_app() added

--- hublib/tool/__init__.py
+++ hublib/tool/__init__.py
@@ -1,2 +1,23 @@
 from .input_types import get_inputs, parse
 from .rw import save, read, rdisplay, run_simtool, DB
+
+import os
+
+# jupyter_notebook_url is set by AppMode extension
+
+def as_app(path):
+    try:
+        dirname = os.path.dirname(jupyter_notebook_url).replace('/notebooks/', '/apps/')
+        path = os.path.join(dirname, path)
+    except:
+        pass
+    return path
+
+
+def as_nb(path):
+    try:
+        dirname = os.path.dirname(jupyter_notebook_url).replace('/apps/', '/notebooks/')
+        path = os.path.join(dirname, path)
+    except:
+        pass
+    return path
